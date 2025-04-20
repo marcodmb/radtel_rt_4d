78059f81f98bdf92df70e61c487c7f10b75f3470
Marco D'Ambrosio <info@marcodambrosio.it>
2025-04-20 12:49:12 +0200

    Update dmr_worldwide_contacts.sh

--- dmr_worldwide_contacts.sh
+++ dmr_worldwide_contacts.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
 curl https://radioid.net/static/user.csv -o user.csv
-cut -d',' -f1,2,3 user.csv > users_mod.csv
-sed -e 's/$/,,,,/' -i users_mod.csv
+curl -O https://raw.githubusercontent.com/ContactLists/NORMAL-ContactLists/refs/heads/main/RADIODDITY/GD77/GD77_WW_default.csv
+
+cut -d',' -f1,2,3 user.csv > users_radioid.csv
+sed -e 's/$/,,,,/' -i users_radioid.csv
+
+cut -d',' -f1,2,3 GD77_WW_default.csv > users_contactlists.csv
+sed -e 's/$/,,,,/' -i users_contactlists.csv
